194014e753ae34599339c66c2b36b36a3dfa3168
Mohammad Abbasi <mxabbasi@gmail.com>
2024-10-30 12:00:52 -0700

    updates routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './containers/home'
+import About from './containers/about'
+import Skills from './containers/skills'
+import Resume from './containers/resume'
+import Portfolio from './containers/portfolio'
+import Contact from './containers/contact'
 
 function App() {
   return (
