6340b8ed7f476c3a994dd350a84e72872e18c39d
Mohammad Abbasi <mxabbasi@gmail.com>
2024-10-30 11:50:28 -0700

    install all dependencies and routing

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,18 @@
-
-
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './containers/home'
 
 function App() {
-  
-
   return (
     <>
-      <h1>Mohammad</h1>
-        
+      <Routes>
+        <Route index path='/' element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/skills' element={<Skills />} />
+      </Routes>
     </>
   )
 }
---
+++ portfolio/src/containers/portfolio/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>
+      PortfolioPage
+    </div>
+  )
+}
+
+export default Portfolio
