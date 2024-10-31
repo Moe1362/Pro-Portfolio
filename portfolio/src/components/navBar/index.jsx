fa24bed5d62ca665f93c4bdad2791faaf251282f
Mohammad Abbasi <mxabbasi@gmail.com>
2024-10-30 17:26:55 -0700

    Create Navbar

--- portfolio/src/components/navBar/index.jsx
+++ portfolio/src/components/navBar/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { FaReact, FaBars } from 'react-icons/fa'
 import { HiX } from 'react-icons/hi'
 import { Link } from 'react-router-dom'
+import './styles.scss'
 
 const data = [
   {
@@ -42,10 +43,10 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="navbar__container">
           <Link to={'./'} className="navbar__container__logo">
-            <FaReact size={30} />
+            <FaReact size={60} />
           </Link>
         </div>
-        <ul className="navbar__container__menu">
+        <ul className={`navbar__container__menu ${toggleIcon ? 'active' : ''}`}>
           {data.map((item, key) => (
             <li key={key} className="navbar__container__menu__item">
               <Link
@@ -58,7 +59,7 @@ const Navbar = () => {
           ))}
         </ul>
         <div className="nav-icon" onClick={handleToggleIcon}>
-          {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+          {toggleIcon ? <HiX size={50} /> : <FaBars size={50} />}
         </div>
       </nav>
     </div>
